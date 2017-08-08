35d2a20008aafd6b26426eeccd03781b775d9dde
adel8268adelad <adelzivari222@gmail.com>
2017-08-08 16:19:17 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,7 +227,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {393853375,95837751},--Sudo users
+    sudo_users = {319684818,95837751},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
